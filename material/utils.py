f4fc8f7288f5a9b837e89c36a54f6e043533f99d
Koimtzoglou <dimitriskoimtzoglou@gmail.com>
2018-05-02 11:56:47 +0200

    Refactoring: Apply changes proposed from Joep

--- material/utils.py
+++ material/utils.py
@@ -4,7 +4,7 @@ from PIL import Image
 from fnmatch import fnmatch
 
 
-def clear_folder(path, unwanted_resolution=(1, 1, 3)):
+def remove_low_resolution_images(path, min_resolution=20):
     """
     A function that removes all the single pixel images
 
@@ -12,17 +12,20 @@ def clear_folder(path, unwanted_resolution=(1, 1, 3)):
     -------------------------
     path: str
         Path to the folder that we would like to clear out.
-    unwanted_resolution: tuple, optional
-        The image resolution that we want to clear out of the folder.
+    min_resolution: int, optional
+        The minimum image resolution that we want to clear out of the folder.
     """
-    print('I am working on the {} folder'.format(path))
+    if not isinstance(min_resolution, int):
+        raise ValueError('The min resolution should be integer')
+    print('I am working on the {} directory'.format(path))
+
     count = 0
     pattern = "*.jpg"
     for dir_path, _, files in os.walk(path):
         for name in files:
             temp_name = os.path.join(dir_path, name)
             temp_shape = np.shape(np.array(Image.open(temp_name)))
-            if temp_shape == unwanted_resolution and fnmatch(temp_name, pattern):
+            if (temp_shape[0]<min_resolution or temp_shape[1]<min_resolution) and fnmatch(temp_name, pattern):
                 os.remove(temp_name)
                 count += 1
             else:
