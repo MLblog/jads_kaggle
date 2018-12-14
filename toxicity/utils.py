5b32b238a77222ccf3a423e66b2318c3caa4d99e
Stergiadis Manos <stergiadis777@gmail.com>
2018-12-14 12:51:22 +0100

    Restructure (#114)
    abstracted generic away into a common directory to be reused by all competition specific sub packages

--- toxicity/utils.py
+++ toxicity/utils.py
@@ -1,27 +1,9 @@
-import time
 import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
 from sklearn import preprocessing
 
 TAGS = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-
-
-def timing(f):
-    """
-    Decorator to time a function call and print results.
-    :param f: Callable to be timed
-    :return: Void. Prints to std:out as a side effect
-    """
-
-    def wrap(*args, **kwargs):
-        start = time.time()
-        ret = f(*args, **kwargs)
-        stop = time.time()
-        print('{} function took {:.1f} seconds to complete\n'.format(f.__name__, (stop - start)))
-        return ret
-
-    return wrap
 
 
 def scale_data(train, test):
