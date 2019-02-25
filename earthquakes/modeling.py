32388afad9916a650b3c34515344eade6536c038
san89 <san_rz89@hotmail.com>
2019-02-25 21:09:02 +0100

    Compute the Short Time Fourier Transform

--- earthquakes/modeling.py
+++ earthquakes/modeling.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import os
 
+from scipy import signal
+
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_absolute_error
 
@@ -41,7 +43,7 @@ def train_and_predict(train_x, train_y, val_x, model_cls, return_model=False, **
 
 def cv_with_feature_computer(data, model_cls, feature_computer, ycol="time_to_failure",
                              n_splits=5, train_samples=1000, val_samples=500, predict_test=False,
-                             data_dir="../data/test", **model_params):
+                             stft=True, data_dir="../data/test", **model_params):
     """Perform custom cross validation using randomly sampled sequences of observations.
 
     Parameters
@@ -67,6 +69,8 @@ def cv_with_feature_computer(data, model_cls, feature_computer, ycol="time_to_fa
     predict_test: boolean, optional (default: False),
         If True, predicts on the test data at every fold and returns (together with cv scores)
         a dataframe with predictions on the test data.
+    stft: boolean, optional (default: True),
+        If true, predicts the Compute the Short Time Fourier Transform
     data_dir: str, optional (default: "../data")
         The path to the main folder with the for this competition data. Note that test data is
         in several files, which are assumed to be in a subfolder called 'test' inside the data_dir.
@@ -93,14 +97,14 @@ def cv_with_feature_computer(data, model_cls, feature_computer, ycol="time_to_fa
 
         # sample random sequences for training
         progress("Sampling {} sequences from training data.".format(train_samples))
-        train_features = create_feature_dataset(train, feature_computer, n_samples=train_samples)
+        train_features = create_feature_dataset(train, feature_computer, n_samples=train_samples, stft=stft)
         y_train = train_features[ycol]
         x_train = train_features.drop(ycol, axis=1)
         progress("Train set sampled.")
 
         # sample random sequences for validation
         progress("Sampling {} sequences from validation data.".format(val_samples))
-        val_features = create_feature_dataset(val, feature_computer, n_samples=val_samples)
+        val_features = create_feature_dataset(val, feature_computer, n_samples=val_samples, stft=stft)
         y_val = val_features[ycol]
         x_val = val_features.drop(ycol, axis=1)
         progress("Validation set sampled.")
@@ -118,9 +122,9 @@ def cv_with_feature_computer(data, model_cls, feature_computer, ycol="time_to_fa
         # predict on test set if specified
         if predict_test:
             if i == 0:
-                test_predictions = predict_on_test(model, feature_computer, data_dir=data_dir)
+                test_predictions = predict_on_test(model, feature_computer, data_dir=data_dir, stft=stft)
             else:
-                new_predictions = predict_on_test(model, feature_computer, data_dir=data_dir)
+                new_predictions = predict_on_test(model, feature_computer, data_dir=data_dir, stft=stft)
                 test_predictions[ycol + "_{}".format(i)] = new_predictions[ycol].copy()
         progress("Predictions on test set made.")
 
@@ -130,7 +134,7 @@ def cv_with_feature_computer(data, model_cls, feature_computer, ycol="time_to_fa
         return scores
 
 
-def predict_on_test(model, feature_computer, ycol="time_to_failure", data_dir="../data"):
+def predict_on_test(model, feature_computer, ycol="time_to_failure", data_dir="../data", stft=True):
     """Load the test data, compute features on every segment, and predict the target.
 
     Parameters
@@ -148,6 +152,8 @@ def predict_on_test(model, feature_computer, ycol="time_to_failure", data_dir=".
         The path to the main folder with the for this competition data. Note that test data is
         in several files, which are assumed to be in a subfolder called 'test' inside the data_dir.
         A file 'sample_submission.csv' is assumed to be directly in data_dir.
+    stft: bool, optional (default: True),
+        To calculate the Short Time Fourier Transform
 
     Returns
     -------
@@ -157,6 +163,10 @@ def predict_on_test(model, feature_computer, ycol="time_to_failure", data_dir=".
     # take the segment ids from the sample submission file
     sample_submission = pd.read_csv(os.path.join(data_dir, "sample_submission.csv"), index_col="seg_id")
     x_test = pd.DataFrame(columns=feature_computer.feature_names, dtype=np.float64, index=sample_submission.index)
+    if stft:
+        x_test_stft= pd.DataFrame(columns=[x + "_stft" for x in feature_computer.feature_names],
+                                  dtype=np.float64,
+                                  index=sample_submission.index)
 
     # load and predict segments one by one
     for i, seg_id in enumerate(x_test.index):
@@ -165,6 +175,13 @@ def predict_on_test(model, feature_computer, ycol="time_to_failure", data_dir=".
 
         segment = pd.read_csv(os.path.join(data_dir, "test", seg_id + ".csv"))
         x_test.loc[seg_id, :] = feature_computer.compute(np.array(segment))
+        if stft:
+            _, _, Zxx = signal.stft([item for sublist in np.array(segment) for item in sublist])
+            x_stft = np.sum(np.abs(Zxx), axis = 0)
+            x_test_stft.loc[seg_id, :] = feature_computer.compute(x_stft)
+    
+    if stft:
+        x_test = pd.concat([x_test, x_test_stft], axis=1)
 
     sample_submission[ycol] = model.predict(x_test)
     progress("Predictions made.")
